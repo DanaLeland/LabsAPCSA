a8a414682c46c61da8eade3535f1e9dcb768d405
DanaLeland <42780588+DanaLeland@users.noreply.github.com>
2018-11-30 10:21:13 -0800

    Add files via upload

---
+++ Lab828/StatTester.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class StatTester{
+    //  Instance variables
+    int[] nums = new int[100];
+    // Constructor
+    public static void main(String[] args){
+        for (int i = 0; i < 100; i++){
+        
+        }
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++){
+        sum = sum + nums[i];
+        }
+        
+    }
+    
+    
+    //  Methods
+    public void loadArray(){
+    
+    }
+    
+}
