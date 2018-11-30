e29e21b2d92d134f316370ff869a735284f7d76a
DanaLeland <42780588+DanaLeland@users.noreply.github.com>
2018-11-30 10:19:14 -0800

    Add files via upload

---
+++ Gibberish/Gibberish.java
@@ -0,0 +1,12 @@
+public class Gibberish
+{
+    public static void main(String args[]){
+    Template template = new Template();
+    Template sourceString = new Template(template.getSourceString());
+    
+    
+    }
+    public String makeGibberish(WordList5000 wordList5000){
+    
+    }
+}
---
+++ Gibberish/GibberishRunner.java
@@ -0,0 +1,12 @@
+public class GibberishRunner extends Gibberish
+
+{
+   public static void main(String args[]){
+       
+       WordList5000 wordList5000 = new WordList5000();
+       Gibberish gibberish = new Gibberish();
+       System.out.println(gibberish.makeGibberish(wordList5000));
+       int currentSourceIndex = 0;
+       System.out.println(makeGibberish);
+    }
+}
